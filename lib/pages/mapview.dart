2363c42d3cfadd93130a4abbc1849d1f845af2cf
Deepanshu <deepanshu.r.sharma@gmail.com>
2024-03-01 21:43:42 +0530

    integrated mapbox api

--- lib/pages/mapview.dart
+++ lib/pages/mapview.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class MapView extends StatefulWidget {
   const MapView({super.key});
@@ -11,22 +11,32 @@ class MapView extends StatefulWidget {
 }
 
 class _MapViewState extends State<MapView> {
-  final Completer<GoogleMapController> _controller =
-      Completer<GoogleMapController>();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Padding(
-      padding: const EdgeInsets.all(15.0),
-      child: GoogleMap(
-        mapType: MapType.normal,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-        initialCameraPosition:
-            CameraPosition(target: LatLng(20.5937, 78.9629), zoom: 5),
-      ),
-    ));
+            padding: const EdgeInsets.all(15.0),
+            child: FlutterMap(
+              options: MapOptions(
+                initialCenter: LatLng(20, 78),
+                initialZoom: 5,
+              ),
+              children: [
+                TileLayer(
+                  urlTemplate:
+                      'https://api.mapbox.com/styles/v1/deepanshu1810/clt8r9qq9006y01qr13915rfg/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZGVlcGFuc2h1MTgxMCIsImEiOiJjbHQ4cG05aHIwdDhiMmlxbXAwbnlndmtnIn0.l_mnRepKUMb8zAy_-YNxkA',
+                  additionalOptions: {
+                    'accessToken':
+                        'pk.eyJ1IjoiZGVlcGFuc2h1MTgxMCIsImEiOiJjbHQ4cG05aHIwdDhiMmlxbXAwbnlndmtnIn0.l_mnRepKUMb8zAy_-YNxkA',
+                    'id': 'mapbox.mapbox-streets-v8'
+                  },
+                ),
+                MarkerLayer(markers: [
+                  Marker(
+                      point: LatLng(20, 78),
+                      child: Image.asset('assets/airplane.png'))
+                ])
+              ],
+            )));
   }
 }
