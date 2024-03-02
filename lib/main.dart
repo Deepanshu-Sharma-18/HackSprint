e94b1ab1837e323fcce26427a2084e39c6e396e0
Deepanshu <deepanshu.r.sharma@gmail.com>
2024-03-02 10:48:52 +0530

    dark theme map

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
-import 'package:flightsky/models/LatLong.dart';
-import 'package:flightsky/models/opensky.dart';
 import 'package:flightsky/pages/mapview.dart';
-import 'package:flightsky/repository/opensky_client.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,25 +13,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  OpenSky? data;
-
-  @override
-  void initState() {
-    fetchOpenSkyData();
-    super.initState();
-  }
-
-  void fetchOpenSkyData() async {
-    data = await OpenSkyClient().fetchOpenSkyData();
-    setState(() {});
-    if (kDebugMode) {
-      if (data != null) {
-        print(data!.states!);
-        // getCoordinates(data!);
-      }
-    }
-  }
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -46,10 +23,6 @@ class _MyAppState extends State<MyApp> {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: data == null
-            ? CircularProgressIndicator()
-            : MapView(
-                list: data!,
-              ));
+        home: MapView());
   }
 }
--- lib/pages/mapview.dart
+++ lib/pages/mapview.dart
@@ -1,35 +1,53 @@
 import 'dart:async';
+import 'dart:ffi';
+import 'dart:io';
 import 'package:flightsky/constants.dart';
 import 'package:flightsky/models/LatLong.dart';
 import 'package:flightsky/models/opensky.dart';
+import 'package:flightsky/repository/opensky_client.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 
 class MapView extends StatefulWidget {
-  final OpenSky list;
-  const MapView({super.key, required this.list});
+  const MapView({super.key});
 
   @override
   State<MapView> createState() => _MapViewState();
 }
 
 class _MapViewState extends State<MapView> {
+  OpenSky? data;
+
+  Future<void> fetchOpenSkyData() async {
+    data = await OpenSkyClient().fetchOpenSkyData();
+    setState(() {});
+    if (kDebugMode) {
+      if (data != null) {
+        getCoordinates(data!);
+        print(data!.states!);
+        // getCoordinates(data!);
+      }
+    }
+  }
+
   List<LatLong> coordinates = [];
   List<Marker> markers = [];
+
+  var refresh = false;
 
   @override
   void initState() {
     super.initState();
-    getCoordinates(widget.list);
+    fetchOpenSkyData();
   }
 
   void getCoordinates(OpenSky data) {
     for (var i = 0; i < data.states!.length; i++) {
       coordinates.add(LatLong(
-        lat: data.states?[i][6] == null ? -1 : data.states![i][6],
-        long: data.states?[i][5] == null ? -1 : data.states![i][5],
+        lat: data.states?[i][6] == null ? -1 : data.states![i][6].toDouble(),
+        long: data.states?[i][5] == null ? -1 : data.states![i][5].toDouble(),
       ));
     }
 
@@ -37,8 +55,15 @@ class _MapViewState extends State<MapView> {
       for (var i = 0; i < coordinates.length; i++) {
         markers.add(Marker(
             point: LatLng(coordinates[i].lat, coordinates[i].long),
-            child: Image.asset(
-              'assets/airplane.png',
+            child: Transform.rotate(
+              angle: data.states?[i][10] == null
+                  ? 0.0
+                  : data.states![i][10].toDouble(),
+              child: Image.asset(
+                'assets/airplane.png',
+                height: 30,
+                width: 30,
+              ),
             )));
       }
       if (kDebugMode) {
@@ -51,28 +76,53 @@ class _MapViewState extends State<MapView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.white60,
+        onPressed: () async {
+          setState(() {
+            refresh = true;
+            markers = [];
+            coordinates = [];
+          });
+          await fetchOpenSkyData();
+          setState(() {
+            refresh = false;
+          });
+        },
+        child: const Icon(Icons.refresh),
+      ),
       body: Padding(
-        padding: const EdgeInsets.all(15.0),
+        padding: const EdgeInsets.all(0),
         // child: Container()));
-        child: FlutterMap(
-          options: MapOptions(
-            initialCenter: LatLng(20, 78),
-            initialZoom: 5,
-          ),
-          children: [
-            TileLayer(
-              urlTemplate:
-                  'https://api.mapbox.com/styles/v1/deepanshu1810/clt8r9qq9006y01qr13915rfg/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZGVlcGFuc2h1MTgxMCIsImEiOiJjbHQ4cG05aHIwdDhiMmlxbXAwbnlndmtnIn0.l_mnRepKUMb8zAy_-YNxkA',
-              additionalOptions: {
-                'accessToken': access_key,
-                'id': 'mapbox.mapbox-streets-v8'
-              },
-            ),
-            MarkerLayer(
-              markers: markers,
-            ),
-          ],
-        ),
+        child: refresh
+            ? Container(
+                height: double.maxFinite,
+                width: double.maxFinite,
+                child: const Center(
+                  child: CircularProgressIndicator(
+                    color: Colors.white,
+                  ),
+                ),
+              )
+            : FlutterMap(
+                options: MapOptions(
+                  initialCenter: LatLng(20, 78),
+                  initialZoom: 5,
+                ),
+                children: [
+                  TileLayer(
+                    urlTemplate:
+                        'https://api.mapbox.com/styles/v1/deepanshu1810/clt9kgyby007x01qr09nuc18h/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZGVlcGFuc2h1MTgxMCIsImEiOiJjbHQ4cG05aHIwdDhiMmlxbXAwbnlndmtnIn0.l_mnRepKUMb8zAy_-YNxkA',
+                    additionalOptions: {
+                      'accessToken': access_key,
+                      'id': 'mapbox.mapbox-streets-v8'
+                    },
+                  ),
+                  MarkerLayer(
+                    markers: markers,
+                  ),
+                ],
+              ),
       ),
     );
   }
